b547c1a5b29b4bd2bc9aabde646279e8f96699a6
trentandraka <stealthbomber10@gmail.com>
2018-10-12 23:25:04 -0400

    New firebase account

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    apiKey: "AIzaSyAavgEMs25EOG7bzQwqaXn4CLC8P5U8geA",
-    authDomain: "uptime-mhacks.firebaseapp.com",
-    databaseURL: "https://uptime-mhacks.firebaseio.com",
-    projectId: "uptime-mhacks",
-    storageBucket: "uptime-mhacks.appspot.com",
-    messagingSenderId: "297348303723"
+    apiKey: "AIzaSyAaQVWqEydHxXtcwaaRyEpiVhJ-vhPJfBo",
+    authDomain: "uptime-69073.firebaseapp.com",
+    databaseURL: "https://uptime-69073.firebaseio.com",
+    projectId: "uptime-69073",
+    storageBucket: "",
+    messagingSenderId: "43889297910"
 }
