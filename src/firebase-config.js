7fb059f51fc0e4ac69ff3609057d056b248eb302
trentandraka <stealthbomber10@gmail.com>
2018-10-12 23:06:40 -0400

    Firebase working

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    apiKey: "AIzaSyAavgEMs25EOG7bzQwqaXn4CLC8P5U8geA",
+    authDomain: "uptime-mhacks.firebaseapp.com",
+    databaseURL: "https://uptime-mhacks.firebaseio.com",
+    projectId: "uptime-mhacks",
+    storageBucket: "uptime-mhacks.appspot.com",
+    messagingSenderId: "297348303723"
+}
